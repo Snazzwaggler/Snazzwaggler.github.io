bc4a1c45c2f9837438b1439c645779d393cd6242
Snazzwaggler <kymkymlarson@gmail.com>
2026-04-13 18:42:49 -0600

    linting

--- app/components/form.js
+++ app/components/form.js
@@ -4,19 +4,16 @@ import { action } from '@ember/object';
 // import emailjs from '@emailjs/browser';
 
 export default class FormComponent extends Component {
+  @tracked username = '';
+  @tracked user_email = '';
+  @tracked message = '';
 
-    @tracked username = "";
-    @tracked user_email = "";
-    @tracked message = "";
+  // SERVICE_ID = "todo";
+  // TEMPLATE_ID = "todod";
 
-    // SERVICE_ID = "todo";
-    // TEMPLATE_ID = "todod";
+  @action sendEmail() {
+    console.log('sending');
 
-    @action sendEmail() {
-        console.log("sending");
-
-        // emailjs.sendForm(this.SERVICE_ID, this.TEMPLATE_ID, )
-
-
-    }
+    // emailjs.sendForm(this.SERVICE_ID, this.TEMPLATE_ID, )
+  }
 }
